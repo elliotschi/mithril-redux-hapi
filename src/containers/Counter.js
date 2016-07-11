580cd3e6ccdf707591838e196d9050e7f802f60a
Elliot S Chi <elliotschi@gmail.com>
2016-07-11 10:39:47 -0700

    feat: add mjsx support

--- src/containers/Counter.js
+++ src/containers/Counter.js
@@ -3,17 +3,38 @@ import m from 'mithril';
 import { connect } from '../configureStore';
 import { increment, decrement } from '../actions/counterActions';
 
+// const Counter = {
+//   view(ctrl, props) {
+//     const { actions: { increment, decrement }, counter } = props;
+
+//     return m('.Counter', [
+//       m('h1', `${counter} clicked`),
+//       m('button', { onclick: increment }, 'increment'),
+//       m('button', { onclick: decrement }, 'decrement')
+//     ]);
+//   }
+// };
+
+// mjsx
 const Counter = {
   view(ctrl, props) {
     const { actions: { increment, decrement }, counter } = props;
 
-    return m('.Counter', [
-      m('h1', `${counter} clicked`),
-      m('button', { onclick: increment }, 'increment'),
-      m('button', { onclick: decrement }, 'decrement')
-    ]);
+    return (
+      <div className="Counter">
+        <h1>
+          {`${counter} clicked`}
+        </h1>
+        <button onclick={increment}>
+          increment
+        </button>
+        <button onclick={decrement}>
+          decrement
+        </button>
+      </div>
+    )
   }
-};
+}
 
 const mapStateToProps = ({ counter }) => ({
   counter
